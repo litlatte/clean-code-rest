853e163bdd152d95ea07675d6608f46eec5b7097
litlatte <hamza.scuola.02@gmail.com>
2021-12-05 12:11:31 +0100

    Updated schema definition

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model(
     'User',
-    mongoose.Schema({
+    new mongoose.Schema({
         username: String,
         password: String
     })
