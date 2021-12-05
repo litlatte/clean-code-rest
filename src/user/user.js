89f46b52461b012dd6164db4572cf680f3fda218
litlatte <hamza.scuola.02@gmail.com>
2021-12-05 15:27:47 +0100

    Bug fix

--- src/user/user.js
+++ src/user/user.js
@@ -1,25 +1,30 @@
 function buildMakeUser(hash, HASH_SIZE, validateUsername, validatePassword) {
   return async ({ username, clearpassword = null, password = null }) => {
-    if (username != `"${(JSON.stringify(username))}"` ) {// This checks if the username is a String
+    if (`"${username}"` != JSON.stringify(username)) {
+      // This checks if the username is a String
       throw new Error("Invalid username");
     }
-    if(!validateUsername(username)){
-        throw new Error("Invalid username");
+    if (!validateUsername(username)) {
+      throw new Error("Invalid username");
     }
     if (!password) {
       if (clearpassword) {
         if (clearpassword != clearpassword.toString()) {
           throw new Error("Invalid password");
         }
-        if(!validatePassword(clearpassword)){
-            throw new Error("Invalid password");
+        if (!validatePassword(clearpassword)) {
+          throw new Error("Invalid password");
         }
         password = await hash(clearpassword);
       } else {
         throw new Error("Invalid password");
       }
     } else {
-      if (password != `"${(JSON.stringify(password))}"` || password.length!=HASH_SIZE) {// The first statement checks if the password is a String
+      if (
+        `"${password}"` != JSON.stringify(password) || // This checks if the password is a String
+        password.length != HASH_SIZE
+      ) {
+        
         throw new Error("Invalid password");
       }
     }
